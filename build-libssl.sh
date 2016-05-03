ef89e9f879aedff0215bfdcd408f1f2f64f6f1ed
Arne Fischer <arne.fischer@letsdev.de>
2016-05-03 23:00:42 +0200

    updated version

--- build-libssl.sh
+++ build-libssl.sh
@@ -21,7 +21,7 @@
 ###########################################################################
 #  Change values here													  #
 #				
-VERSION="1.0.2d"													      #
+VERSION="1.0.2h"													      #
 SDKVERSION=`xcrun -sdk iphoneos --show-sdk-version`														  #
 CONFIG_OPTIONS=""
 CURL_OPTIONS=""
