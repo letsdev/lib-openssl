b079cb0fe6beca3dc16c33fe88e13d02735dcb1e
Martin Schwamberger <martin.schwamberger@letsdev.de>
2019-06-07 10:18:21 +0200

    Update substidution. Set ios sdk 12.2

--- build-android.sh
+++ build-android.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -e
 
 VERSION=$1
 
-if [ -z $VERSION ]; then
+if [[ -z ${VERSION} ]]; then
     echo "Missing VERSION as first parameter"
     exit 99
 fi
@@ -21,7 +21,7 @@ ANDROID_SDK=21
 ## Variables
 ## --------------------
 
-if [ ! -d "${ANDROID_NDK_HOME}" ]; then
+if [[ ! -d "${ANDROID_NDK_HOME}" ]]; then
   echo "ANDROID_NDK_HOME not defined or directory does not exist"
   exit 1
 fi
@@ -49,14 +49,14 @@ function clear_android_env {
 }
 
 function build_android_arch {
-    log_title "$1"
-    if [ -z $1 ]; then
+    log_title "${1}"
+    if [[ -z $1 ]]; then
         echo 'no ABI set'
         exit 1
-        elif [ -z $2 ]; then
+        elif [[ -z $2 ]]; then
         echo 'no ARCH set'
         exit 1
-        elif [ -z $3 ]; then
+        elif [[ -z $3 ]]; then
         echo 'no toolchain name set'
         exit 1
     fi
@@ -65,42 +65,43 @@ function build_android_arch {
     local TOOLCHAIN_NAME=$3
     local COMPILER=$4
 
-    local SRC_DIR=${BUILD_DIR}/android-$ABI
-    local LOG_FILE="$SRC_DIR/android-$ABI-$VERSION.log"
+    local SRC_DIR=${BUILD_DIR}/android-${ABI}
+    local LOG_FILE="$SRC_DIR/android-${ABI}-${VERSION}.log"
 
     local TOOL_NAME=${ARCH}-linux-android
-    local TOOLCHAIN_ROOT_PATH=$BUILD_DIR/toolchains/${TOOL_NAME}
-    local TOOLCHAIN_PATH=$TOOLCHAIN_ROOT_PATH/bin
-    local NDK_TOOLCHAIN_BASENAME=$TOOLCHAIN_PATH/$TOOLCHAIN_NAME
+    local TOOLCHAIN_ROOT_PATH=${BUILD_DIR}/toolchains/${TOOL_NAME}
+    local TOOLCHAIN_PATH=${TOOLCHAIN_ROOT_PATH}/bin
+    local NDK_TOOLCHAIN_BASENAME=${TOOLCHAIN_PATH}/${TOOLCHAIN_NAME}
     echo "NDK_TOOLCHAIN_BASENAME ${NDK_TOOLCHAIN_BASENAME}"
 
     # indicate new build
     echo ">>>"
     echo "Begin: $(date)"
 	# folder, zip, target, target dir
-    unarchive $OPENSSL_NAME $OPENSSL_PATH "android-$ABI" $SRC_DIR
+    unarchive ${OPENSSL_NAME} ${OPENSSL_PATH} "android-${ABI}" ${SRC_DIR}
+    patch ${SRC_DIR}
     
-    if [ -d $TOOLCHAIN_PATH ]; then
+    if [[ -d ${TOOLCHAIN_PATH} ]]; then
         echo "toolchain ${TOOL_NAME} exists"
     else
         echo "toolchain ${TOOL_NAME} missing, create it"
-        $ANDROID_NDK_HOME/build/tools/make-standalone-toolchain.sh \
-        --platform=android-$ANDROID_SDK \
+        ${ANDROID_NDK_HOME}/build/tools/make-standalone-toolchain.sh \
+        --platform=android-${ANDROID_SDK} \
         --stl=libc++_shared \
-        --arch=$ARCH \
-        --install-dir=$TOOLCHAIN_ROOT_PATH \
+        --arch=${ARCH} \
+        --install-dir=${TOOLCHAIN_ROOT_PATH} \
         --verbose
     fi
 
-    export SYSROOT=$TOOLCHAIN_ROOT_PATH/sysroot
-    export CC="$NDK_TOOLCHAIN_BASENAME-clang --sysroot=${SYSROOT}"
-    export CXX=$NDK_TOOLCHAIN_BASENAME-clang++
+    export SYSROOT=${TOOLCHAIN_ROOT_PATH}/sysroot
+    export CC="${NDK_TOOLCHAIN_BASENAME}-clang --sysroot=${SYSROOT}"
+    export CXX=${NDK_TOOLCHAIN_BASENAME}-clang++
     export LINK=${CXX} 
-    export LD=$NDK_TOOLCHAIN_BASENAME-ld
-    export AR=$NDK_TOOLCHAIN_BASENAME-ar
-    export AS=$NDK_TOOLCHAIN_BASENAME-as
-    export RANLIB=$NDK_TOOLCHAIN_BASENAME-ranlib
-    export STRIP=$NDK_TOOLCHAIN_BASENAME-strip
+    export LD=${NDK_TOOLCHAIN_BASENAME}-ld
+    export AR=${NDK_TOOLCHAIN_BASENAME}-ar
+    export AS=${NDK_TOOLCHAIN_BASENAME}-as
+    export RANLIB=${NDK_TOOLCHAIN_BASENAME}-ranlib
+    export STRIP=${NDK_TOOLCHAIN_BASENAME}-strip
     
     export ARCH_FLAGS=$5
     export ARCH_LINK=$6 
@@ -109,14 +110,14 @@ function build_android_arch {
     export CFLAGS=" ${ARCH_FLAGS} -fPIC -ffunction-sections -funwind-tables -fstack-protector -fno-strict-aliasing " 
     export LDFLAGS=" ${ARCH_LINK} "
 
-	echo "Configuring android-$ABI"
-	(cd "$SRC_DIR"; ./Configure $OPENSSL_CONFIG_OPTIONS -DOPENSSL_PIC -fPIC "$COMPILER" > "$LOG_FILE" 2>&1)
+	echo "Configuring android-${ABI}"
+	(cd "${SRC_DIR}"; ./Configure ${OPENSSL_CONFIG_OPTIONS} -DOPENSSL_PIC -fPIC "${COMPILER}" > "${LOG_FILE}" 2>&1)
 
-    echo "Building android-$ABI..."
-	(cd "$SRC_DIR"; make build_libs >> "$LOG_FILE" 2>&1)
+    echo "Building android-${ABI}..."
+	(cd "${SRC_DIR}"; make build_libs >> "${LOG_FILE}" 2>&1)
 
 	clear_android_env
-    check_files $ABI $LOG_FILE
+    check_files ${ABI} ${LOG_FILE}
     echo "Finished: $(date)"
 }
 
@@ -131,22 +132,22 @@ function build_android {
 
 	# abi, arch, toolchain, openssl-config, arch_flags, arch_link
 	build_android_arch 'arm64-v8a' 'arm64' 'aarch64-linux-android' 'android'
-	build_android_arch 'armeabi-v7a' 'arm' 'arm-linux-androideabi' 'android-armv7' $ARMV7_ARCH_FLAGS $ARMV7_ARCH_LINK
-	build_android_arch 'armeabi' 'arm' 'arm-linux-androideabi' 'android' $ARM_ARCH_FLAGS
-	build_android_arch 'x86' 'x86' 'i686-linux-android' 'android-x86' $X86_ARCH_FLAGS
+	build_android_arch 'armeabi-v7a' 'arm' 'arm-linux-androideabi' 'android-armv7' ${ARMV7_ARCH_FLAGS} ${ARMV7_ARCH_LINK}
+	build_android_arch 'armeabi' 'arm' 'arm-linux-androideabi' 'android' ${ARM_ARCH_FLAGS}
+	build_android_arch 'x86' 'x86' 'i686-linux-android' 'android-x86' ${X86_ARCH_FLAGS}
 	build_android_arch 'x86_64' 'x86_64' 'x86_64-linux-android' 'android'
 }
 
 function check_files() {
-    local FILE_SSL=$BUILD_DIR/android-$1/libssl.a
-    local FILE_CRYPTO=$BUILD_DIR/android-$1/libcrypto.a
-    if [ ! -f $FILE_SSL ]; then
-        echo "Missing $FILE_SSL"
+    local FILE_SSL=${BUILD_DIR}/android-$1/libssl.a
+    local FILE_CRYPTO=${BUILD_DIR}/android-$1/libcrypto.a
+    if [[ ! -f ${FILE_SSL} ]]; then
+        echo "Missing ${FILE_SSL}"
         cat $2
         exit 1
     fi
-    if [ ! -f $FILE_CRYPTO ]; then
-        echo "Missing $FILE_CRYPTO"
+    if [[ ! -f ${FILE_CRYPTO} ]]; then
+        echo "Missing ${FILE_CRYPTO}"
         cat $2
         exit 2
     fi
@@ -157,19 +158,19 @@ function distribute_android {
     log_title "Distribute Android"
 
     local PLATFORM="Android"
-    local NAME="$PLATFORM"
-    local DIR="$DIST_DIR/$NAME/openssl"
+    local NAME="${PLATFORM}"
+    local DIR="${DIST_DIR}/${NAME}/openssl"
 	local ANDROID_ABIS="arm64-v8a armeabi-v7a armeabi x86 x86_64"
 	
-	for ABI in $ANDROID_ABIS; do
-		local ABI_DIR=$DIR/$ABI
-    	mkdir -p $ABI_DIR/include
-    	mkdir -p $ABI_DIR/lib
+	for ABI in ${ANDROID_ABIS}; do
+		local ABI_DIR=${DIR}/${ABI}
+    	mkdir -p ${ABI_DIR}/include
+    	mkdir -p ${ABI_DIR}/lib
 
-        echo "Copy $ABI"
-		cp -LR $BUILD_DIR/android-$ABI/include/* $ABI_DIR/include
-		cp $BUILD_DIR/android-$ABI/libcrypto.a $ABI_DIR/lib/libcrypto.a
-		cp $BUILD_DIR/android-$ABI/libssl.a $ABI_DIR/lib/libssl.a
+        echo "Copy ${ABI}"
+		cp -LR ${BUILD_DIR}/android-${ABI}/include/* ${ABI_DIR}/include
+		cp ${BUILD_DIR}/android-${ABI}/libcrypto.a ${ABI_DIR}/lib/libcrypto.a
+		cp ${BUILD_DIR}/android-${ABI}/libssl.a ${ABI_DIR}/lib/libssl.a
     done
 }
 
--- build-ios.sh
+++ build-ios.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -e
 
 VERSION=$1
 
-if [ -z $VERSION ]; then
+if [[ -z ${VERSION} ]]; then
     echo "Missing VERSION as first parameter"
     exit 99
 fi
@@ -15,21 +15,21 @@ download_openssl
 ## Parameters
 ## --------------------
 
-IOS_SDK=12.0
+IOS_SDK=12.2
 MIN_IOS=7.0
-IOS_ARCHS="i386 x86_64 armv7 armv7s arm64"
+IOS_ARCHS="i386 x86_64 armv7 armv7s arm64 arm64e"
 
 ## --------------------
 ## Variables
 ## --------------------
 
 DEVELOPER_DIR=`xcode-select -print-path`
-if [ ! -d $DEVELOPER_DIR ]; then
-    echo "Please set up Xcode correctly. '$DEVELOPER_DIR' is not a valid developer tools folder."
+if [[ ! -d ${DEVELOPER_DIR} ]]; then
+    echo "Please set up Xcode correctly. '${DEVELOPER_DIR}' is not a valid developer tools folder."
     exit 1
 fi
-if [ ! -d "$DEVELOPER_DIR/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS$IOS_SDK.sdk" ]; then
-    echo "The iOS SDK $IOS_SDK was not found."
+if [[ ! -d "${DEVELOPER_DIR}/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS${IOS_SDK}.sdk" ]]; then
+    echo "The iOS SDK ${IOS_SDK} was not found."
     exit 1
 fi
 
@@ -42,8 +42,9 @@ function build_ios() {
 	log_title "iOS Build"
 	echo "Using iOS SDK ${IOS_SDK}"
 	echo "Using iOS min version ${MIN_IOS}"
-    
-	for ARCH in $IOS_ARCHS; do
+	echo "Xcode Developer directory ${DEVELOPER_DIR}"
+
+	for ARCH in ${IOS_ARCHS}; do
         log_title "$ARCH"
         local PLATFORM="iPhoneOS"
         local COMPILER="iphoneos-cross"
@@ -52,22 +53,22 @@ function build_ios() {
             PLATFORM="iPhoneSimulator"
         fi
 
-        local SRC_DIR="$BUILD_DIR/$PLATFORM-$ARCH"
-        local LOG_FILE="$SRC_DIR/$PLATFORM$IOS_SDK-$ARCH.log"
+        local SRC_DIR="${BUILD_DIR}/${PLATFORM}-${ARCH}"
+        local LOG_FILE="${SRC_DIR}/${PLATFORM}${IOS_SDK}-${ARCH}.log"
         
-        export CROSS_TOP="$DEVELOPER_DIR/Platforms/$PLATFORM.platform/Developer"
-        export CROSS_SDK="$PLATFORM$IOS_SDK.sdk"
-        export CC="clang -arch $ARCH -fembed-bitcode"
+        export CROSS_TOP="${DEVELOPER_DIR}/Platforms/${PLATFORM}.platform/Developer"
+        export CROSS_SDK="${PLATFORM}${IOS_SDK}.sdk"
+        export CC="clang -arch ${ARCH} -fembed-bitcode"
         # indicate new build
         echo ">>>"
 		# folder, zip, target, target dir
-        unarchive $OPENSSL_NAME $OPENSSL_PATH "$PLATFORM-$ARCH" $SRC_DIR
+        unarchive ${OPENSSL_NAME} ${OPENSSL_PATH} "${PLATFORM}-${ARCH}" ${SRC_DIR}
 
-   		echo "Configuring $PLATFORM-$ARCH..."
-        (cd "$SRC_DIR"; ./Configure $OPENSSL_CONFIG_OPTIONS "$COMPILER" > "$LOG_FILE" 2>&1)
+   		echo "Configuring ${PLATFORM}-${ARCH}..."
+        (cd "${SRC_DIR}"; ./Configure ${OPENSSL_CONFIG_OPTIONS} "${COMPILER}" > "${LOG_FILE}" 2>&1)
         
         # Patch Makefile
-        if [ "$ARCH" == "x86_64" ]; then
+        if [[ "${ARCH}" == "x86_64" ]]; then
             sed -ie "s/^CFLAG= -/CFLAG=  -miphoneos-version-min=$MIN_IOS -DOPENSSL_NO_ASM -/" "$SRC_DIR/Makefile"
         else
             sed -ie "s/^CFLAG= -/CFLAG=  -miphoneos-version-min=$MIN_IOS -/" "$SRC_DIR/Makefile"
@@ -76,8 +77,8 @@ function build_ios() {
         #sed -ie "s/^# define OPENSSL_VERSION_NUMBER.*$/# define OPENSSL_VERSION_NUMBER  $FAKE_NIBBLE/" "$SRC_DIR/crypto/opensslv.h"
         #sed -ie "s/^#  define OPENSSL_VERSION_TEXT.*$/#  define OPENSSL_VERSION_TEXT  \"$FAKE_TEXT\"/" "$SRC_DIR/crypto/opensslv.h"
         
-    	echo "Building $PLATFORM-$ARCH..."
-    	(cd "$SRC_DIR"; make >> "$LOG_FILE" 2>&1)
+    	echo "Building ${PLATFORM}-${ARCH}..."
+    	(cd "${SRC_DIR}"; make >> "${LOG_FILE}" 2>&1)
 
 		unset CROSS_TOP
 		unset CROSS_SDK
@@ -89,49 +90,49 @@ function distribute_ios() {
     log_title "Distribute iOS"
 
     local PLATFORM="iOS"
-    local NAME="$PLATFORM"
-    local DIR="$DIST_DIR/$NAME/openssl"
+    local NAME="${PLATFORM}"
+    local DIR="${DIST_DIR}/${NAME}/openssl"
     local FILES="libcrypto.a libssl.a"
-    mkdir -p "$DIR/include"
-    mkdir -p "$DIR/lib"
+    mkdir -p "${DIR}/include"
+    mkdir -p "${DIR}/lib"
     
     #echo "$VERSION" > "$DIR/VERSION"
     #cp "$BUILD_DIR/iPhoneSimulator-i386/LICENSE" "$DIR"
-    cp -LR "$BUILD_DIR/iPhoneSimulator-i386/include/" "$DIR/include"
+    cp -LR "${BUILD_DIR}/iPhoneSimulator-i386/include/" "${DIR}/include"
     
     # Alter rsa.h to make Swift happy
-    sed -i .bak 's/const BIGNUM \*I/const BIGNUM *i/g' "$DIR/include/openssl/rsa.h"
+    sed -i .bak 's/const BIGNUM \*I/const BIGNUM *i/g' "${DIR}/include/openssl/rsa.h"
     
     echo "Combine library files"
-    for f in $FILES; do
-        local OUTPUT_FILE=$DIR/lib/$f
+    for f in ${FILES}; do
+        local OUTPUT_FILE=${DIR}/lib/${f}
         lipo -create \
-        "$BUILD_DIR/iPhoneSimulator-i386/$f" \
-        "$BUILD_DIR/iPhoneSimulator-x86_64/$f" \
-        "$BUILD_DIR/iPhoneOS-arm64/$f" \
-        "$BUILD_DIR/iPhoneOS-armv7/$f" \
-        "$BUILD_DIR/iPhoneOS-armv7s/$f" \
-        -output $OUTPUT_FILE
-        echo "Created $OUTPUT_FILE"
+        "${BUILD_DIR}/iPhoneSimulator-i386/${f}" \
+        "${BUILD_DIR}/iPhoneSimulator-x86_64/${f}" \
+        "${BUILD_DIR}/iPhoneOS-arm64/${f}" \
+        "${BUILD_DIR}/iPhoneOS-armv7/${f}" \
+        "${BUILD_DIR}/iPhoneOS-armv7s/${f}" \
+        -output ${OUTPUT_FILE}
+        echo "Created ${OUTPUT_FILE}"
     done
 
 	echo "Create iOS-Framework"
-	local FRAMEWORK_DIR=$DIST_DIR/Framework-iOS
-	mkdir -p $FRAMEWORK_DIR/Openssl.framework/Headers
- 	mkdir -p $FRAMEWORK_DIR/Ssl.framework/Headers
-	mkdir -p $FRAMEWORK_DIR/Crypto.framework
+	local FRAMEWORK_DIR=${DIST_DIR}/Framework-iOS
+	mkdir -p ${FRAMEWORK_DIR}/Openssl.framework/Headers
+ 	mkdir -p ${FRAMEWORK_DIR}/Ssl.framework/Headers
+	mkdir -p ${FRAMEWORK_DIR}/Crypto.framework
 
-	cp -LR $DIR/include/openssl/ $FRAMEWORK_DIR/Openssl.framework/Headers/
-	cp -LR $DIR/include/openssl/ $FRAMEWORK_DIR/Ssl.framework/Headers/
+	cp -LR ${DIR}/include/openssl/ ${FRAMEWORK_DIR}/Openssl.framework/Headers/
+	cp -LR ${DIR}/include/openssl/ ${FRAMEWORK_DIR}/Ssl.framework/Headers/
 
-	copy "$DIR/lib/libssl.a" "$FRAMEWORK_DIR/Openssl.framework/ssl"
-	copy "$DIR/lib/libssl.a" "$FRAMEWORK_DIR/Ssl.framework/ssl"
-	copy "$DIR/lib/libcrypto.a" "$FRAMEWORK_DIR/Openssl.framework/crypto"
-	copy "$DIR/lib/libcrypto.a" "$FRAMEWORK_DIR/Crypto.framework/crypto"
+	copy "${DIR}/lib/libssl.a" "${FRAMEWORK_DIR}/Openssl.framework/ssl"
+	copy "${DIR}/lib/libssl.a" "${FRAMEWORK_DIR}/Ssl.framework/ssl"
+	copy "${DIR}/lib/libcrypto.a" "${FRAMEWORK_DIR}/Openssl.framework/crypto"
+	copy "${DIR}/lib/libcrypto.a" "${FRAMEWORK_DIR}/Crypto.framework/crypto"
 }
 
 function copy() {
-    echo "Copy $1 => $2"
+    echo "Copy ${1} => ${2}"
     cp $1 $2
 }
 
