be342f65a5aaa58bb24d136fd2d9064151467ed5
Arne Fischer <arne.fischer@letsdev.de>
2023-02-07 15:01:01 +0100

    Changed android back to static lib.

--- build-android.sh
+++ build-android.sh
@@ -166,8 +166,11 @@ function distribute_android {
 
         echo "Copy ${ABI}"
 		cp -LR ${BUILD_DIR}/android-${ABI}/include/* ${ABI_DIR}/include
-		cp ${BUILD_DIR}/android-${ABI}/libcrypto.so.1.1 ${ABI_DIR}/lib/libcrypto.so
-		cp ${BUILD_DIR}/android-${ABI}/libssl.so.1.1 ${ABI_DIR}/lib/libssl.so
+        ## in case we want to go back to shared libs, leave this here.
+		#cp ${BUILD_DIR}/android-${ABI}/libcrypto.so.1.1 ${ABI_DIR}/lib/libcrypto.so
+		#cp ${BUILD_DIR}/android-${ABI}/libssl.so.1.1 ${ABI_DIR}/lib/libssl.so
+		cp ${BUILD_DIR}/android-${ABI}/libcrypto.a ${ABI_DIR}/lib/libcrypto.a
+		cp ${BUILD_DIR}/android-${ABI}/libssl.a ${ABI_DIR}/lib/libssl.a
     done
 }
 
